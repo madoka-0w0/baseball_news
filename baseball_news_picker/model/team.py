cb1c5dfad27781db40247bca5b023f6e1d26cae5
madoka <mad106.t@gmail.com>
2019-04-29 22:35:04 +0900

    add teams

--- baseball_news_picker/model/team.py
+++ baseball_news_picker/model/team.py
@@ -6,6 +6,13 @@ class Team:
     HANSHIN = "阪神"
     HIROSHIMA = "広島"
 
+    SEIBU = "西武"
+    NICHIHAMU = "日本ハム"
+    LOTTE = "ロッテ"
+    ORIX = "オリックス"
+    SOFTBANK = "ソフトバンク"
+    RAKUTEN = "楽天"
+
     __team_number_dict = {
         YOMIURI: 1,
         TOKYO: 2,
@@ -13,6 +20,12 @@ class Team:
         CHUNICHI: 4,
         HANSHIN: 5,
         HIROSHIMA: 6,
+        SEIBU: 7,
+        NICHIHAMU: 8,
+        LOTTE: 9,
+        ORIX: 11,
+        SOFTBANK: 12,
+        RAKUTEN: 376
     }
     __team_name_dict = {
         "巨": YOMIURI,
@@ -21,6 +34,12 @@ class Team:
         "中": CHUNICHI,
         "神": HANSHIN,
         "広": HIROSHIMA,
+        "西": SEIBU,
+        "日": NICHIHAMU,
+        "ロ": LOTTE,
+        "オ": ORIX,
+        "ソ": SOFTBANK,
+        "楽": RAKUTEN,
     }
 
     @staticmethod
